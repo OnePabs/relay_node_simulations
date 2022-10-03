14079dc2a709a1ea9cb4acf720756456ca59deb1
OnePabs <juanpablocontreras1313@gmail.com>
2022-10-03 00:54:32 -0400

    script to create and save neural network that predicts wheter technique A or C must be used. Start of predictive simulator that loads the model

--- create_training_dataset_for_predictive.py
+++ create_training_dataset_for_predictive.py
@@ -10,99 +10,149 @@ else:
     print("different number of first part and second part experiments")
     exit()
 n_per_cycle = 50
-load_factor = 1
-dist = MultipleBarsExponential(mean_ia_for_first_part_of_cycle[0], mean_ia_for_second_part_of_cycle[0], n_per_cycle, load_factor)
+load_factors = [1, 2/3, 0.5]
+dist = MultipleBarsConstant(mean_ia_for_first_part_of_cycle[0],
+                            mean_ia_for_second_part_of_cycle[0],
+                            n_per_cycle,
+                            load_factors[0])
 
 m = 1000                # number of files per graph
 n = 50                  # number of requests per file
 num_input_nodes = 5     # number of input nodes for the predictive nodes.
 num_points_averaged_per_input_node = int(n/num_input_nodes)  # number of req averaged for each node = n/num_input_nodes
-path = r"C:\Users\juanp\OneDrive\Documents\experiments\predictive-training\data.csv"
+inputs_path = r"C:\Users\juanp\OneDrive\Documents\experiments\predictive-training\inputs_test.csv"
+labels_path = r"C:\Users\juanp\OneDrive\Documents\experiments\predictive-training\labels_test.csv"
 
 # write headers
-f = open(path,"w")
+f_inputs = open(inputs_path, "w")
 for i in range(num_input_nodes):
-    f.write("input_"+str(i)+",")
-f.write("prediction\n")
-f.close()
+    f_inputs.write("input_"+str(i))
+    if i < num_input_nodes - 1:
+        f_inputs.write(",")
+f_inputs.write("\n")
+f_inputs.close()
 
+f_labels = open(labels_path, "w")
+f_labels.write("labels\n")
+f_labels.close()
 
-# all same inter arrival time
-for k in range(num_settings):
-    dist.change_settings(mean_ia_for_first_part_of_cycle[k],
-                         mean_ia_for_second_part_of_cycle[k],
-                         n_per_cycle,
-                         load_factor)
-    for i in range(m):
-        data = dist.create(n, write_to_file=False)
-        # get and write averages
-        f = open(path, "a")
-        for num_input_nodes_idx in range(num_input_nodes):
-            avg = Sim_math_ops.average(
-                data[
-                    num_input_nodes_idx*num_points_averaged_per_input_node:
-                    (num_input_nodes_idx+1)*num_points_averaged_per_input_node
-                ])
-            f.write(str(avg)+",")
-        if k == 0:
-            f.write("1")
-        else:
-            f.write("0")
-        f.write("\n")
-        f.close()
+for load_factor_idx in range(len(load_factors)):
+    for k in range(num_settings):
+        dist.change_settings(mean_ia_for_first_part_of_cycle[k],
+                             mean_ia_for_second_part_of_cycle[k],
+                             n_per_cycle,
+                             load_factors[load_factor_idx])
+        for i in range(m):
+            data = dist.create(n, write_to_file=False)
+            # get and write inputs (averages)
+            f_inputs = open(inputs_path, "a")
+            for num_input_nodes_idx in range(num_input_nodes):
+                avg = Sim_math_ops.average(
+                    data[
+                    num_input_nodes_idx * num_points_averaged_per_input_node:
+                    (num_input_nodes_idx + 1) * num_points_averaged_per_input_node
+                    ])
+                f_inputs.write(str(avg))
+                if num_input_nodes_idx < num_input_nodes-1:
+                    f_inputs.write(",")
+            f_inputs.write("\n")
+            f_inputs.close()
+            # get and write labels (predictions)
+            f_labels = open(labels_path, "a")
+            if load_factor_idx == 0:
+                if k == 0:
+                    f_labels.write("1")
+                else:
+                    f_labels.write("0")
+            elif load_factor_idx == 1:
+                if k == 0:
+                    f_labels.write("0")
+                else:
+                    f_labels.write("1")
+            elif load_factor_idx == 2:
+                if k == 0:
+                    f_labels.write("0")
+                else:
+                    f_labels.write("1")
+            f_labels.write("\n")
+            f_labels.close()
 
-
-# two thirds
-load_factor = 2/3
-for k in range(num_settings):
-    dist.change_settings(mean_ia_for_first_part_of_cycle[k],
-                         mean_ia_for_second_part_of_cycle[k],
-                         n_per_cycle,
-                         load_factor)
-    for i in range(m):
-        data = dist.create(n, write_to_file=False)
-        # get and write averages
-        f = open(path, "a")
-        for num_input_nodes_idx in range(num_input_nodes):
-            avg = Sim_math_ops.average(
-                data[
-                num_input_nodes_idx * num_points_averaged_per_input_node:
-                (num_input_nodes_idx + 1) * num_points_averaged_per_input_node
-                ])
-            f.write(str(avg) + ",")
-        if k == 0:
-            f.write("0")
-        else:
-            f.write("1")
-        f.write("\n")
-        f.close()
-
-
-# # half
-load_factor = 0.5
-for k in range(num_settings):
-    dist.change_settings(mean_ia_for_first_part_of_cycle[k],
-                         mean_ia_for_second_part_of_cycle[k],
-                         n_per_cycle,
-                         load_factor)
-    for i in range(m):
-        data = dist.create(n, write_to_file=False)
-        # get and write averages
-        f = open(path, "a")
-        for num_input_nodes_idx in range(num_input_nodes):
-            avg = Sim_math_ops.average(
-                data[
-                num_input_nodes_idx * num_points_averaged_per_input_node:
-                (num_input_nodes_idx + 1) * num_points_averaged_per_input_node
-                ])
-            f.write(str(avg) + ",")
-        if k == 0:
-            f.write("0")
-        else:
-            f.write("1")
-        f.write("\n")
-        f.close()
-
+# # all same inter arrival time
+# for k in range(num_settings):
+#     dist.change_settings(mean_ia_for_first_part_of_cycle[k],
+#                          mean_ia_for_second_part_of_cycle[k],
+#                          n_per_cycle,
+#                          load_factor)
+#     for i in range(m):
+#         data = dist.create(n, write_to_file=False)
+#         # get and write averages
+#         f = open(path, "a")
+#         for num_input_nodes_idx in range(num_input_nodes):
+#             avg = Sim_math_ops.average(
+#                 data[
+#                     num_input_nodes_idx*num_points_averaged_per_input_node:
+#                     (num_input_nodes_idx+1)*num_points_averaged_per_input_node
+#                 ])
+#             f.write(str(avg)+",")
+#         if k == 0:
+#             f.write("1")
+#         else:
+#             f.write("0")
+#         f.write("\n")
+#         f.close()
+#
+#
+# # two thirds
+# load_factor = 2/3
+# for k in range(num_settings):
+#     dist.change_settings(mean_ia_for_first_part_of_cycle[k],
+#                          mean_ia_for_second_part_of_cycle[k],
+#                          n_per_cycle,
+#                          load_factor)
+#     for i in range(m):
+#         data = dist.create(n, write_to_file=False)
+#         # get and write averages
+#         f = open(path, "a")
+#         for num_input_nodes_idx in range(num_input_nodes):
+#             avg = Sim_math_ops.average(
+#                 data[
+#                 num_input_nodes_idx * num_points_averaged_per_input_node:
+#                 (num_input_nodes_idx + 1) * num_points_averaged_per_input_node
+#                 ])
+#             f.write(str(avg) + ",")
+#         if k == 0:
+#             f.write("0")
+#         else:
+#             f.write("1")
+#         f.write("\n")
+#         f.close()
+#
+#
+# # # half
+# load_factor = 0.5
+# for k in range(num_settings):
+#     dist.change_settings(mean_ia_for_first_part_of_cycle[k],
+#                          mean_ia_for_second_part_of_cycle[k],
+#                          n_per_cycle,
+#                          load_factor)
+#     for i in range(m):
+#         data = dist.create(n, write_to_file=False)
+#         # get and write averages
+#         f = open(path, "a")
+#         for num_input_nodes_idx in range(num_input_nodes):
+#             avg = Sim_math_ops.average(
+#                 data[
+#                 num_input_nodes_idx * num_points_averaged_per_input_node:
+#                 (num_input_nodes_idx + 1) * num_points_averaged_per_input_node
+#                 ])
+#             f.write(str(avg) + ",")
+#         if k == 0:
+#             f.write("0")
+#         else:
+#             f.write("1")
+#         f.write("\n")
+#         f.close()
+#
 
 # note:
 
---
+++ predictive_nn.py
@@ -0,0 +1,73 @@
+import numpy as np
+import keras
+from keras import models
+from keras.utils import to_categorical
+from keras.utils.vis_utils import model_to_dot
+from sklearn.model_selection import train_test_split
+
+BATCH_SIZE = 100
+EPOCHS = 10
+
+# Load data
+inputs_path = r"C:\Users\juanp\OneDrive\Documents\experiments\predictive-training\inputs_test.csv"
+labels_path = r"C:\Users\juanp\OneDrive\Documents\experiments\predictive-training\labels_test.csv"
+
+input_data = np.genfromtxt(inputs_path, delimiter=',')
+labels_data = np.genfromtxt(labels_path, delimiter=',')
+
+# delete headers
+input_data = np.delete(input_data, 0, 0)
+labels_data = np.delete(labels_data, 0, 0)
+
+np.delete(labels_data, 0, 0)
+
+
+print("inputs shape: " + str(input_data.shape))
+print("labels shape: " + str(labels_data.shape))
+
+# Split dataset into train (50%), validation (25%), and test (25%)
+X_train, X_rem, y_train, y_rem = train_test_split(input_data, labels_data, train_size=0.5)
+X_valid, X_test, y_valid, y_test = train_test_split(X_rem, y_rem, test_size=0.8)
+
+print("X train shape: " + str(X_train.shape))
+print("Y train shape: " + str(y_train.shape))
+print("X X_test shape: " + str(X_test.shape))
+print("Y y_test shape: " + str(y_test.shape))
+print("X X_valid shape: " + str(X_valid.shape))
+print("Y y_valid shape: " + str(y_valid.shape))
+
+# create and specify the structure of the model
+model = models.Sequential()
+model.add(keras.layers.Dense(128, input_shape=(5,)))      # batches of 8. innput data of size (num_points,5)
+model.add(keras.layers.Dense(32, activation='relu'))
+model.add(keras.layers.Dense(32, activation='relu'))
+model.add(keras.layers.Dense(32, activation='relu'))
+model.add(keras.layers.Dense(1, activation='sigmoid'))
+
+#compile the model
+model.compile(optimizer='Adam', loss='binary_crossentropy', metrics=["accuracy"])
+
+
+#train the model
+print()
+history = model.fit(X_train, y_train, batch_size=8, epochs=10, validation_data=(X_valid, y_valid))
+print(history.history)
+
+# save the model
+save_model_path = r"C:\Users\juanp\OneDrive\Documents\experiments\predictive_models\predictive_nn"
+model.save(save_model_path)
+
+# Evaluate model on Test data
+print()
+results = model.evaluate(X_test, y_test, batch_size=8)
+print('test loss, test acc:', results)
+
+# predict one value
+print()
+prediction = model.predict([[50, 50, 50, 50, 50]])
+print("prediction: ", prediction)
+
+# Summary of neural network
+# model.summary()
+
+
